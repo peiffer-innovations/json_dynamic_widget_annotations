7a4d6cb92f356e26d66074d54b82eaff993a484b
Jeff Peiffer <jpeiffer@gmail.com>
2023-08-07 18:24:44 -0400

    Updates

--- lib/json_dynamic_widget_annotations.dart
+++ lib/json_dynamic_widget_annotations.dart
@@ -1,7 +1,24 @@
-const jsonWidget = JsonWidget();
+const jsonWidget = JsonWidget(
+  autoRegister: true,
+);
+const jsonWidgetRegistrar = JsonWidgetRegistrar();
 
 class JsonBuilder {
   const JsonBuilder();
+}
+
+class JsonBuilderParam {
+  const JsonBuilderParam();
+}
+
+class JsonParamAlias {
+  const JsonParamAlias({
+    required this.alias,
+    required this.name,
+  });
+
+  final String alias;
+  final String name;
 }
 
 class JsonParamDecoder {
@@ -16,6 +33,46 @@ class JsonParamSchema {
   final String param;
 }
 
+class JsonPositionedParam {
+  const JsonPositionedParam(this.name);
+
+  final String name;
+}
+
+class JsonSchemaName {
+  const JsonSchemaName(this.name);
+
+  final String name;
+}
+
 class JsonWidget {
-  const JsonWidget();
+  const JsonWidget({
+    this.autoRegister = true,
+    this.widget,
+  });
+
+  final bool autoRegister;
+  final String? widget;
+}
+
+class JsonWidgetRegistrar {
+  const JsonWidgetRegistrar();
+}
+
+enum NumSupportedChildren {
+  many,
+  one,
+  zero;
+}
+
+class JsonWidgetRegistration {
+  const JsonWidgetRegistration({
+    required this.builder,
+    this.schema = '',
+    required this.widget,
+  });
+
+  final String builder;
+  final String schema;
+  final String widget;
 }
